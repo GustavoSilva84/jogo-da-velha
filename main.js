fdf2ba8324db76c315ff2d8335e323d1baaaab7f
Gustavo-twi <72147773+Gustavo-Silva-01@users.noreply.github.com>
2020-10-26 10:44:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // LIMPAR TABULEIRO
 const BUTTON_JOGO = document.querySelectorAll('.button_jogo');         
-const FUNDO_GANHADOR_TEXT = document.querySelector('#fundo_ganhador_text');
+const GANHADOR = document.querySelector('#fundo_ganhador')
 
 BUTTON_JOGO.forEach(BUTTON_JOGO => {
   BUTTON_JOGO.innerHTML = '';
@@ -8,7 +8,6 @@ BUTTON_JOGO.forEach(BUTTON_JOGO => {
 
 var vez_do_jogador = 0;
 var simbolos = ['X','O'];
-
 var tabuleiro_matriz = [
     ['','',''],
     ['','',''],
@@ -60,7 +59,7 @@ function checar(){
     if(tabuleiro_matriz[i][0] == simbolos[0] && tabuleiro_matriz[i][1] == simbolos[0] && tabuleiro_matriz[i][2] == simbolos[0]) {                         
       
       if(tabuleiro_matriz[i][0] != ''){
-        FUNDO_GANHADOR_TEXT.innerHTML = '<p class="text_ganhador"> Ganhador X </p>'
+        GANHADOR.innerHTML = '<p class="ganhador"> Ganhador X </p>'
         jogo = true
         return
       }
@@ -68,7 +67,7 @@ function checar(){
     }else if(tabuleiro_matriz[i][0] == simbolos[1] && tabuleiro_matriz[i][1] == simbolos[1] && tabuleiro_matriz[i][2] == simbolos[1]) {
      
       if(tabuleiro_matriz[i][0] != '') {
-        FUNDO_GANHADOR_TEXT.innerHTML = '<p class="text_ganhador"> Ganhador O </p>'
+        GANHADOR.innerHTML = '<p class="ganhador"> Ganhador O </p>'
         jogo = true
         return
       }
@@ -79,7 +78,7 @@ function checar(){
     if(tabuleiro_matriz[0][i] == simbolos[0] && tabuleiro_matriz[1][i] == simbolos[0] && tabuleiro_matriz[2][i] == simbolos[0]) {
       
       if(tabuleiro_matriz[0][i] != '') {
-        FUNDO_GANHADOR_TEXT.innerHTML =  '<p class="text_ganhador"> Ganhador X </p>'
+        GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador X </p>'
         jogo = true
         return
       }
@@ -87,7 +86,7 @@ function checar(){
     }else if(tabuleiro_matriz[0][i] == simbolos[1] && tabuleiro_matriz[1][i] == simbolos[1] && tabuleiro_matriz[2][i] == simbolos[1]){
       
       if(tabuleiro_matriz[0][i] != '') {
-        FUNDO_GANHADOR_TEXT.innerHTML =  '<p class="text_ganhador"> Ganhador O </p>'
+        GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador O </p>'
         jogo = true
         return
       }
@@ -100,7 +99,7 @@ function checar(){
   if(tabuleiro_matriz[0][0] == simbolos[0] && tabuleiro_matriz[1][1] == simbolos[0] && tabuleiro_matriz[2][2] == simbolos[0]) {
   
     if(tabuleiro_matriz[0][0] != '') {
-      FUNDO_GANHADOR_TEXT.innerHTML =  '<p class="text_ganhador"> Ganhador X </p>'
+      GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador X </p>'
       jogo = true
       return
     }
@@ -108,7 +107,7 @@ function checar(){
   }else if(tabuleiro_matriz[0][0] == simbolos[1] && tabuleiro_matriz[1][1] == simbolos[1] && tabuleiro_matriz[2][2] == simbolos[1]){
     
     if(tabuleiro_matriz[0][0] != '') {
-      FUNDO_GANHADOR_TEXT.innerHTML =  '<p class="text_ganhador"> Ganhador O </p>'
+      GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador O </p>'
       jogo = true
       return
     }
@@ -119,7 +118,7 @@ function checar(){
   if(tabuleiro_matriz[0][2] == simbolos[0] && tabuleiro_matriz[1][1] == simbolos[0] && tabuleiro_matriz[2][0] == simbolos[0]) {
     
     if(tabuleiro_matriz[0][2] != '') {
-      FUNDO_GANHADOR_TEXT.innerHTML =  '<p class="text_ganhador"> Ganhador X </p>'
+      GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador X </p>'
       jogo = true
       return
     }
@@ -127,7 +126,7 @@ function checar(){
   }else if(tabuleiro_matriz[0][2] == simbolos[1] && tabuleiro_matriz[1][1] == simbolos[1] && tabuleiro_matriz[2][0] == simbolos[1]) {
     
     if(tabuleiro_matriz[0][2] != '') {
-      FUNDO_GANHADOR_TEXT.innerHTML =  '<p class="text_ganhador"> Ganhador O </p>';
+      GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador O </p>';
       jogo = true;
       return
     }
@@ -137,7 +136,7 @@ function checar(){
 }
 
 function mostrar_text_empate(){
-  FUNDO_GANHADOR_TEXT.innerHTML = '<p class="text_ganhador"> Empate </p>';
+  GANHADOR.innerHTML = '<p class="ganhador"> Empate </p>';
 }
 function empate(){
   for(let r = 0; r < 3; r++){
