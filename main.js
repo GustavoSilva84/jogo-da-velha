0827cb9d4b21e3f60597e9415f546e0dca1f2b75
Gustavo-twi <72147773+Gustavo-Silva-01@users.noreply.github.com>
2020-10-27 10:00:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,150 +1,188 @@
-// LIMPAR TABULEIRO
-const BUTTON_JOGO = document.querySelectorAll('.button_jogo');         
-const GANHADOR = document.querySelector('#fundo_ganhador')
-
-BUTTON_JOGO.forEach(BUTTON_JOGO => {
-  BUTTON_JOGO.innerHTML = '';
-});
-
-var vez_do_jogador = 0;
-var simbolos = ['X','O'];
+var vez_jogador = 'x';
+var vencedor_x = 0
+var vencedor_o = 0
+var button_iniciar_jogo = document.querySelector('.button_iniciar_jogo')
 var tabuleiro_matriz = [
-    ['','',''],
-    ['','',''],
-    ['','',''],
+  ['','',''],
+  ['','',''],
+  ['','',''],
 ];
 
-var jogo = false;
+var jogando = false;
 
-function button_click_jogo(row, column) {
+function iniciar_reiniciar_jogo(){
+  jogando = true;
   
-  if(jogo === false && tabuleiro_matriz[row][column] == ''){
+  button_iniciar_jogo.innerHTML = 'Jogo iniciado'
+  button_iniciar_jogo.style.background = '#00FF83'
+  button_iniciar_jogo.style.color= 'white'
+  button_iniciar_jogo.style.fontWeight = '400'
   
-    let id = `${row}_${column}`;
-    
-    if(vez_do_jogador === 0) {
-      
-      if(vez_do_jogador == 0) {
-        
-        document.getElementById(id).innerHTML = `<p class="x"> X </p>`;
-        
-      }else {
-        
-        document.getElementById(id).innerHTML = `<p class="k"> O </p>`;
-        
-      }
-      
-      tabuleiro_matriz[row][column] = 'X'
-      vez_do_jogador = 1;
-      
-    }else {
-      
-      document.getElementById(id).innerHTML = simbolos[vez_do_jogador];              
-      tabuleiro_matriz[row][column] = 'O'
-      vez_do_jogador = 0;
-      
-    }
-    
-    checar()
-    
-  }
+  // modo de reiniciar joga sera adicionando em breve!
   
+  return;
 }
 
-function checar(){
+function click_button_game(row, column){
   
-  for(let i = 0; i < 3; i++){
+  if(jogando === true){
     
-    // CHECAR LINHAS
-    if(tabuleiro_matriz[i][0] == simbolos[0] && tabuleiro_matriz[i][1] == simbolos[0] && tabuleiro_matriz[i][2] == simbolos[0]) {                         
-      
-      if(tabuleiro_matriz[i][0] != ''){
-        GANHADOR.innerHTML = '<p class="ganhador"> Ganhador X </p>'
-        jogo = true
-        return
-      }
-      
-    }else if(tabuleiro_matriz[i][0] == simbolos[1] && tabuleiro_matriz[i][1] == simbolos[1] && tabuleiro_matriz[i][2] == simbolos[1]) {
-     
-      if(tabuleiro_matriz[i][0] != '') {
-        GANHADOR.innerHTML = '<p class="ganhador"> Ganhador O </p>'
-        jogo = true
-        return
-      }
-      
-    }          
+    let vez_do_jogador = document.querySelector('.vez_do_jogador');                        
+    let texto_vez_do_jogador = document.querySelector('.texto_vez_do_jogador');
+    let button_game = document.querySelector('.button_game');
+    let id = `row${row}_column${column}`;
     
-    // CHECAR COLUNAS
-    if(tabuleiro_matriz[0][i] == simbolos[0] && tabuleiro_matriz[1][i] == simbolos[0] && tabuleiro_matriz[2][i] == simbolos[0]) {
+    if(tabuleiro_matriz[row][column] === ''){
       
-      if(tabuleiro_matriz[0][i] != '') {
-        GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador X </p>'
-        jogo = true
-        return
-      }
-      
-    }else if(tabuleiro_matriz[0][i] == simbolos[1] && tabuleiro_matriz[1][i] == simbolos[1] && tabuleiro_matriz[2][i] == simbolos[1]){
-      
-      if(tabuleiro_matriz[0][i] != '') {
-        GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador O </p>'
-        jogo = true
-        return
+      if(vez_jogador === 'x'){
+        document.getElementById(id).innerHTML = '<p id="x"> X </p>';
+        
+        tabuleiro_matriz[row][column] = 'x';
+        
+        texto_vez_do_jogador.innerHTML = 'VEZ DO O';
+        button_game.style.color = '#12B4FF';
+        vez_do_jogador.style.background = '#FF125E';
+        vez_jogador = 'o';
+      }else{
+        document.getElementById(id).innerHTML = '<p id="o"> O </p>';
+        
+        tabuleiro_matriz[row][column] = 'o';
+        
+        texto_vez_do_jogador.innerHTML = 'VEZ DO X';
+        button_game.style.color = '#FF125E';
+        vez_do_jogador.style.background = '#12B4FF';
+        vez_jogador = 'x';
       }
       
     }
-   
+    checar_ganhador()
   }
-  
-  // DIAGONAL ESQUERDA
-  if(tabuleiro_matriz[0][0] == simbolos[0] && tabuleiro_matriz[1][1] == simbolos[0] && tabuleiro_matriz[2][2] == simbolos[0]) {
-  
-    if(tabuleiro_matriz[0][0] != '') {
-      GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador X </p>'
-      jogo = true
-      return
-    }
-    
-  }else if(tabuleiro_matriz[0][0] == simbolos[1] && tabuleiro_matriz[1][1] == simbolos[1] && tabuleiro_matriz[2][2] == simbolos[1]){
-    
-    if(tabuleiro_matriz[0][0] != '') {
-      GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador O </p>'
-      jogo = true
-      return
-    }
-    
-  }
-  
-  // DIAGONAL DIREITA
-  if(tabuleiro_matriz[0][2] == simbolos[0] && tabuleiro_matriz[1][1] == simbolos[0] && tabuleiro_matriz[2][0] == simbolos[0]) {
-    
-    if(tabuleiro_matriz[0][2] != '') {
-      GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador X </p>'
-      jogo = true
-      return
-    }
-    
-  }else if(tabuleiro_matriz[0][2] == simbolos[1] && tabuleiro_matriz[1][1] == simbolos[1] && tabuleiro_matriz[2][0] == simbolos[1]) {
-    
-    if(tabuleiro_matriz[0][2] != '') {
-      GANHADOR.innerHTML =  '<p class="ganhador"> Ganhador O </p>';
-      jogo = true;
-      return
-    }
-    
-  }
-  empate();
 }
 
-function mostrar_text_empate(){
-  GANHADOR.innerHTML = '<p class="ganhador"> Empate </p>';
-}
-function empate(){
-  for(let r = 0; r < 3; r++){
-    for(let c = 0; c < 3; c++){
-      if(tabuleiro_matriz[r][c] == ''){
-        return
+function checar_ganhador(){
+  
+  if(jogando === true){
+    
+    for(let check_row = 0; check_row < 3; check_row++){
+      for(let check_column = 0; check_column < 3; check_column++){
+        
+        if(tabuleiro_matriz[check_row][0] === tabuleiro_matriz[check_row][1] && tabuleiro_matriz[check_row][0] == tabuleiro_matriz[check_row][2]){                      
+          if(tabuleiro_matriz[check_row][0] != ''){
+            
+            if(vez_jogador != 'o'){
+              
+              let pontuacao_o = document.querySelector('.pontuacao_o');
+              
+              vencedor_o++;
+              pontuacao_o.innerHTML = `O = ${vencedor_o}`
+              jogando = false;
+              
+              return;
+              
+            }else if(vez_jogador != 'x'){
+              
+              let pontuacao_x = document.querySelector('.pontuacao_x')
+              
+              vencedor_x++
+              pontuacao_x.innerHTML = `X = ${vencedor_x}`
+              jogando = false;
+              
+              return;
+              
+            }
+            
+          }
+        }
+        
+        if (tabuleiro_matriz[0][check_column] === tabuleiro_matriz[1][check_column] && tabuleiro_matriz[1][check_column] == tabuleiro_matriz[1][check_column]) {
+          if (tabuleiro_matriz[0][check_column] != '') {
+        
+            if (vez_jogador != 'o') {
+        
+              let pontuacao_o = document.querySelector('.pontuacao_o');
+        
+              vencedor_o++;
+              pontuacao_o.innerHTML = `O = ${vencedor_o}`
+              jogando = false;
+              
+              return;
+        
+            } else if (vez_jogador != 'x') {
+        
+              let pontuacao_x = document.querySelector('.pontuacao_x')
+        
+              vencedor_x++
+              pontuacao_x.innerHTML = `X = ${vencedor_x}`
+              jogando = false;
+              
+              return;
+        
+            }
+        
+          }
+        }
+        
       }
     }
+    
+    if(tabuleiro_matriz[0][0] == tabuleiro_matriz[1][1] && tabuleiro_matriz[0][0] == tabuleiro_matriz[2][2]){
+      if(tabuleiro_matriz[0][0] != ''){
+        
+        if (vez_jogador != 'o') {
+        
+          let pontuacao_o = document.querySelector('.pontuacao_o');
+        
+          vencedor_o++;
+          pontuacao_o.innerHTML = `O = ${vencedor_o}`
+          jogando = false;
+        
+          
+        
+        } else if (vez_jogador != 'x') {
+        
+          let pontuacao_x = document.querySelector('.pontuacao_x')
+        
+          vencedor_x++
+          pontuacao_x.innerHTML = `X = ${vencedor_x}`
+          jogando = false;
+        
+          return;
+        
+        }
+       
+      }
+    }
+    
+    
+    if (tabuleiro_matriz[0][2] == tabuleiro_matriz[1][1] && tabuleiro_matriz[0][2] == tabuleiro_matriz[2][0]) {
+      if (tabuleiro_matriz[0][2] != '') {
+    
+        if (vez_jogador != 'o') {
+    
+          let pontuacao_o = document.querySelector('.pontuacao_o');
+    
+          vencedor_o++;
+          pontuacao_o.innerHTML = `O = ${vencedor_o}`
+          jogando = false;
+    
+          return;
+    
+        } else if (vez_jogador != 'x') {
+    
+          let pontuacao_x = document.querySelector('.pontuacao_x')
+    
+          vencedor_x++
+          pontuacao_x.innerHTML = `X = ${vencedor_x}`
+          jogando = false;
+    
+          return;
+    
+        }
+    
+      }
+    }
+    
   }
-  mostrar_text_empate()
+  
 }
+
